949f6fd0a938ee450ffdf5ef692403b825e6ee05
mdarif980 <arifpass980@gmail.com>
2026-06-30 10:28:46 +0600

    Update omni_TECNO-KH6.mk

--- omni_TECNO-KH6.mk
+++ omni_TECNO-KH6.mk
@@ -5,10 +5,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Inherit common Omni stuff
 $(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit device specific
 $(call inherit-product, device/tecno/TECNO-KH6/device.mk)
 
 PRODUCT_DEVICE := TECNO_KH6
@@ -16,10 +16,3 @@ PRODUCT_NAME := omni_TECNO_KH6
 PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO-KH6
 PRODUCT_MANUFACTURER := TECNO
-
-PRODUCT_GMS_CLIENTID_BASE := android-tecno
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vnd_kh6_h6513-user 12 SP1A.210812.016 353056 release-keys"
-
-BUILD_FINGERPRINT := TECNO/KH6-OP/TECNO-KH6:12/SP1A.210812.016/230613V1347:user/release-keys
