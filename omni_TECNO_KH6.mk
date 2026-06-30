15e7eb02e0812a989e1821c8e328d46feec55f05
mdarif980 <arifpass980@gmail.com>
2026-06-30 15:52:44 +0600

    Delete omni_TECNO_KH6.mk

--- omni_TECNO_KH6.mk
+++
@@ -1,8 +0,0 @@
-# Inherit device specific
-$(call inherit-product, device/tecno/TECNO-KH6/device.mk)
-
-PRODUCT_DEVICE := TECNO_KH6
-PRODUCT_NAME := omni_TECNO_KH6
-PRODUCT_BRAND := TECNO
-PRODUCT_MODEL := TECNO-KH6
-PRODUCT_MANUFACTURER := TECNO
